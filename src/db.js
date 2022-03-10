0b701d46950431442c583d2d150e851bdbcf013b
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-03-09 20:31:39 -0500

    feat: Config database

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./mock.db', sqlite3.OPEN_READWRITE, err => {
   if (err) return console.error(err.message);
-  console.log('connected');
+  // console.log('connected');
 });
 
 module.exports = db;
