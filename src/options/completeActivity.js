34694b19f58fca85c59f89901645e0f96fc11bf5
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-04-30 23:25:32 -0500

    feat: Update table name in update query

--- src/options/completeActivity.js
+++ src/options/completeActivity.js
@@ -2,33 +2,33 @@ const { prompt } = require('inquirer');
 const db = require('../config/db');
 
 function completeActivity() {
-  db.all('SELECT * FROM actividad', (err, rows) => {
+  db.all('SELECT * FROM activity', (err, rows) => {
     prompt([
       {
         type: 'list',
         name: 'id',
         message: 'Seleccione la actividad',
-        choices: rows.map(e => e.actividad_id + '-' + e.nombre),
+        choices: rows.map(e => e.activity_id + '-' + e.name),
         default: 0,
       },
     ]).then(res => {
       db.all(
-        `SELECT * FROM link where actividad_id =${
+        `SELECT * FROM reference where activity_id =${
           res.id.split('-')[0]
-        } and finalizado = 0`,
+        } and completed = 0`,
         (err, rows) => {
           prompt([
             {
               type: 'checkbox',
               name: 'complete',
-              message: 'Seleccione los links',
-              choices: rows.map(e => e.link_id + '-' + e.url),
+              message: 'Seleccione los references',
+              choices: rows.map(e => e.reference_id + '-' + e.url),
               default: 0,
             },
           ]).then(select => {
             if (select.complete?.length > 0) {
               const stmt = db.prepare(
-                "UPDATE link set finalizado=1, fecha_finalizado=datetime('now','localtime') where link_id = ?"
+                "UPDATE reference set completed=1, completed_at=datetime('now','localtime') where reference_id = ?"
               );
               select.complete.forEach(element => {
                 stmt.run(element.split('-')[0]);
@@ -40,6 +40,7 @@ function completeActivity() {
       );
     });
   });
+  db.close();
 }
 
 module.exports = { completeActivity };
