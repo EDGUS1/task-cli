f79a92e0e550dddf8f7e20745ca5017c4f184b55
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-04-30 23:41:18 -0500

    fix: Execute insert from in sql script

--- src/commands/config.js
+++ src/commands/config.js
@@ -1,22 +1,30 @@
 let fs = require('fs');
+const path = require('node:path');
+
 const db = require('../config/db');
-// const db = require('./config/db');
+const { log_error } = require('../utils/print');
 
-const configdb = async () => {
-  fs.readFile('./src/schema.sqlite3', 'utf-8', (err, data) => {
-    if (err) {
-      console.log('error: ', err);
-    } else {
-      data
-        .split(';')
-        .map(e => e.replace(/\n|\r/g, ''))
-        .forEach(e => db.run(e));
-
-      db.close(err => {
-        if (err) return console.error(err.message);
-      });
+const configdb = () => {
+  fs.readFile(
+    path.join(__dirname, '..', 'schema.sqlite3'),
+    'utf-8',
+    (err, data) => {
+      if (err) {
+        console.log('error: ', err);
+      } else {
+        db.serialize(() => {
+          data
+            .split(';')
+            .map(e => e.replace(/\n|\r/g, ''))
+            .forEach(e => db.run(e));
+        });
+        console.log('Objetos de DB creados correctamente');
+        db.close(err => {
+          if (err) return log_error(err.message);
+        });
+      }
     }
-  });
+  );
 };
 
 module.exports = { configdb };
