ea32fd52d54462f5e6a190752245119e6ba5e746
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-04-30 23:27:47 -0500

    feat: Change function name to completeActvDaily

--- src/options/updateActivity.js
+++ src/options/updateActivity.js
@@ -1,31 +1,36 @@
 const { prompt } = require('inquirer');
 const db = require('../config/db');
 
-function updateActivity() {
+function completeActvDaily() {
   db.all(
-    'SELECT * FROM actividad where tipo_prioridad_id = 0 and actividad_id NOT IN (SELECT actividad_id from bitacora)',
+    'SELECT activity_id, name FROM activity where type_priority_id = 2 and activity_id NOT IN (SELECT activity_id from bitacora)',
     (err, rows) => {
-      prompt([
-        {
-          type: 'checkbox',
-          name: 'complete',
-          message: 'Seleccione la(s) actividad(es)',
-          choices: rows.map(e => e.actividad_id + '-' + e.nombre),
-          default: 0,
-        },
-      ]).then(select => {
-        if (select.complete?.length > 0) {
-          const stmt = db.prepare(
-            "INSERT into bitacora (actividad_id, hecho, fecha_creacion, fecha_hecho) VALUES (?,1,datetime('now','localtime'),datetime('now','localtime'))"
-          );
-          select.complete.forEach(element => {
-            stmt.run(element.split('-')[0]);
-          });
-          stmt.finalize();
-        }
-      });
+      if (rows && rows.length > 0) {
+        prompt([
+          {
+            type: 'checkbox',
+            name: 'complete',
+            message: 'Seleccione la(s) actividad(es)',
+            choices: rows.map(e => e.activity_id + '-' + e.name),
+            default: 0,
+          },
+        ]).then(select => {
+          if (select.complete?.length > 0) {
+            const stmt = db.prepare(
+              "INSERT into bitacora (activity_id, completed, completed_at) VALUES (?,1,datetime('now','localtime'))"
+            );
+            select.complete.forEach(element => {
+              stmt.run(element.split('-')[0]);
+            });
+            stmt.finalize();
+          }
+        });
+      } else {
+        console.log('Todas las actividades han sido completadas');
+      }
     }
   );
+  db.close();
 }
 
-module.exports = { updateActivity };
+module.exports = { completeActvDaily };
