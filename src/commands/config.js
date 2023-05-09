5a0d0d9bbe4f66012ab070437f439e0f49e8e073
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-05-08 22:41:56 -0500

    feat: Change implementation to promise

--- src/commands/config.js
+++ src/commands/config.js
@@ -1,30 +1,26 @@
 let fs = require('fs');
 const path = require('node:path');
 
-const db = require('../config/db');
-const { log_error } = require('../utils/print');
-
-const configdb = () => {
-  fs.readFile(
-    path.join(__dirname, '..', 'schema.sqlite3'),
-    'utf-8',
-    (err, data) => {
-      if (err) {
-        console.log('error: ', err);
-      } else {
-        db.serialize(() => {
-          data
-            .split(';')
-            .map(e => e.replace(/\n|\r/g, ''))
-            .forEach(e => db.run(e));
-        });
-        console.log('Objetos de DB creados correctamente');
-        db.close(err => {
-          if (err) return log_error(err.message);
+const configdb = database => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, '..', 'schema.sqlite3'),
+      'utf-8',
+      (err, data) => {
+        if (err) reject(err.message);
+        database.serialize(() => {
+          const d = data.split(';');
+          let t = 0;
+          d.map(e => e.replace(/\n|\r/g, '')).forEach(q => {
+            database.run(q, function (err) {
+              if (err) reject(err.message);
+              if (d.length == ++t) resolve('Objetos creados correctamente');
+            });
+          });
         });
       }
-    }
-  );
+    );
+  });
 };
 
 module.exports = { configdb };
