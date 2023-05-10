1498c767d673fb8c1062a84aaa04f8e74aff603d
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-05-09 23:36:12 -0500

    feat: Create new functions to get activities

--- src/options/database/activity.js
+++ src/options/database/activity.js
@@ -1,6 +1,6 @@
 function getAllActivities(database) {
   return new Promise((resolve, reject) => {
-    let sql = `SELECT * FROM activity`;
+    const sql = `SELECT * FROM activity`;
     return database.all(sql, function (err, res) {
       if (err) return reject(err.message);
       return resolve(res);
@@ -10,7 +10,7 @@ function getAllActivities(database) {
 
 function getAllActiveActivities(database) {
   return new Promise((resolve, reject) => {
-    let sql = `SELECT a.name, a.description, t.name AS priority FROM activity a INNER JOIN type_priority t ON a.type_priority_id = t.type_priority_id WHERE a.active = 1`;
+    const sql = `SELECT a.activity_id, a.name, a.description, t.name AS priority FROM activity a LEFT JOIN type_priority t ON a.type_priority_id = t.type_priority_id WHERE a.active = 1`;
     return database.all(sql, function (err, res) {
       if (err) return reject(err.message);
       return resolve(res);
@@ -20,7 +20,7 @@ function getAllActiveActivities(database) {
 
 function getAllDailyActivities(database) {
   return new Promise((resolve, reject) => {
-    let sql = `SELECT a.name, a.description, (CASE WHEN ((SELECT b.completed FROM bitacora b WHERE b.active = 1 AND b.activity_id = a.activity_id) IS NULL) THEN 'TODO' ELSE 'COMPLETED' END) AS estado FROM activity a WHERE a.active = 1 AND a.type_priority_id = 2`;
+    const sql = `SELECT a.name, a.description, (CASE WHEN ((SELECT b.completed FROM bitacora b WHERE b.active = 1 AND b.activity_id = a.activity_id AND created_at <> datetime('now','localtime')) IS NULL) THEN 'TODO' ELSE 'COMPLETED' END) AS estado FROM activity a WHERE a.active = 1 AND a.type_priority_id = 2`;
     return database.all(sql, function (err, res) {
       if (err) return reject(err.message);
       return resolve(res);
@@ -30,7 +30,17 @@ function getAllDailyActivities(database) {
 
 function getActivityById(database, id) {
   return new Promise((resolve, reject) => {
-    let sql = `SELECT * FROM activity WHERE activity_id = ?`;
+    const sql = `SELECT * FROM activity WHERE activity_id = ?`;
+    return database.get(sql, [id], function (err, res) {
+      if (err) return reject(err.message);
+      return resolve(res);
+    });
+  });
+}
+
+function getActiveActivityById(database, id) {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT type_priority_id, name, description FROM activity WHERE active = 1 AND activity_id = ?`;
     return database.get(sql, [id], function (err, res) {
       if (err) return reject(err.message);
       return resolve(res);
@@ -69,6 +79,26 @@ function deleteActivity(database, id) {
   });
 }
 
+function getAllActiveIncompleteDailyActivities(database) {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT activity_id, name FROM activity WHERE active = 1 AND type_priority_id = 2 and activity_id NOT IN (SELECT activity_id FROM bitacora)`;
+    return database.all(sql, function (err, res) {
+      if (err) return reject(err.message);
+      return resolve(res);
+    });
+  });
+}
+
+function getLastIdInserted(database) {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT MAX(activity_id) as id FROM activity`;
+    return database.get(sql, function (err, res) {
+      if (err) return reject(err.message);
+      return resolve(res);
+    });
+  });
+}
+
 module.exports = {
   getActivityById,
   getAllActivities,
@@ -77,4 +107,7 @@ module.exports = {
   insertActivity,
   updateActivity,
   deleteActivity,
+  getAllActiveIncompleteDailyActivities,
+  getActiveActivityById,
+  getLastIdInserted,
 };
