bcd6abe5eb6fb8d286ec5e2bebc59ead8c513e3e
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-05-09 23:39:49 -0500

    feat: Extract print_table function

--- src/utils/print.js
+++ src/utils/print.js
@@ -1,23 +1,31 @@
 const { Color } = require('./color');
 
+const COLOR_ = 3;
+const BACKGROUND = 4;
+
 const custom_print = (msg, opt, color) => {
   process.stdout.write(`\x1b[${opt}8;5;${color}m ${msg} \x1b[m`);
 };
 
 const log = msg => {
-  custom_print(msg, 4, Color.black);
+  custom_print(msg, BACKGROUND, Color.black);
 };
 
 const log_error = msg => {
-  custom_print(msg, 4, Color.red);
+  custom_print(msg, BACKGROUND, Color.red);
 };
 
 const log_warn = msg => {
-  custom_print(msg, 3, Color.yellow);
+  custom_print(msg, COLOR_, Color.yellow);
 };
 
 const log_info = msg => {
-  custom_print(msg, 3, Color.blue);
+  custom_print(msg, COLOR_, Color.blue);
 };
 
-module.exports = { log, log_error, log_info, log_warn };
+function print_table(rows) {
+  if (rows && rows.length > 0) console.table(rows, Object.keys(rows[0]));
+  else log_error('No hay datos guardados');
+}
+
+module.exports = { log, log_error, log_info, log_warn, print_table };
