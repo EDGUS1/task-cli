e87f466c56f5bb3ef7d4d660461beba15cdd75e7
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-05-10 20:29:39 -0500

    chore: Rename file

--- src/config/createDB.js
+++ src/config/createDB.js
@@ -1,10 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const create_db = dbname => {
+const createDB = dbname => {
   return new sqlite3.Database(dbname, sqlite3.OPEN_READWRITE, err => {
     if (err) return console.error(err.message, dbname);
     // console.log('connected');
   });
 };
 
-module.exports = create_db;
+module.exports = createDB;
